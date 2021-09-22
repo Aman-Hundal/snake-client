dd167fc2aa958ec76af814582d2266918037c3c3
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-09-22 23:11:36 +0000

    added additonal functionality, and refactored code using module.exports ES6 shorthanding (object desctrction and key short handing

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.223.133",
+    port: 50542,
+  });
+
+  //to provide a message to client (ON CONNECTION EVENT) that we have connected to the server +  to send a 3LETTER name to server in the form of "Name: 123"
+  conn.on("connect", () => {
+    console.log("Connected to the game server. Have fun!");
+    conn.write("Name: AMO"); // sends information to the server on connection. The server reads this format of Name: ___ and creates a name for you.
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // handle incoming data from server
+  conn.on('data', (data) => {
+    console.log('Server Says:', data);
+  });
+
+  return conn; //represents the connection you have with the server. This conn object will be used to handle msgs from the server.
+};
+
+// console.log("Connecting ...");
+// connect(); //sets up connection to the game server and interperts incoming text as UTF. Essentially does all at once. Previously we broke down setEncoding and createConnetion as their own functions and calls
+// // we will be adding all of our client side code in the conn object esentialy. Such as receiveing events from the server as well
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: 50542,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // handle incomine data from server
-  conn.on('data', (data) => {
-    console.log('Server Says:', data);
-  });
-
-  return conn; //represents the connection you have with the server. This conn object will be used to handle msgs from the server.
-};
+const {connect} = require('./client.js'); // this imports the connect funciton from client.js AND CALLS IT without the need to be called in this script... WhHY???
 
 console.log("Connecting ...");
-connect(); //sets up connection to the game server and interperts incoming text as UTF. Essentially does all at once. Previously we broke down setEncoding and createConnetion as their own functions and calls
-// we will be adding all of our client side code in the conn object esentialy. Such as receiveing events from the server as well
+connect();
